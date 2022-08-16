2d0eeba7c0b24918b4f09258b0e4181836cf70b4
Sego <sego@sego.app>
2022-08-16 02:49:59 +0200

    version 1.1.0 minor update

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
+const version = '1.1.0'
 const fs = require('fs')
+const download = require('download')
 const chalk = require('chalk');
 const inquirer = require('inquirer')
-
+const decompress = require('decompress');
+const axios = require('axios');
+const url = `https://github.com/SegoGithub/oof-is-back/releases/download/v1.1.0/sounds.zip`
 console.log(chalk.bgCyan('Welcome to "Oof is back!"'))
 
 fs.readdirSync(`${process.env.LOCALAPPDATA}\\Roblox\\Versions`).forEach(file => {
@@ -21,14 +25,27 @@ fs.readdirSync(`${process.env.LOCALAPPDATA}\\Roblox\\Versions`).forEach(file => 
                     'Half life',
                     'Old Minecraft Death Sound',
                     'Lego Yoda Death',
-                    'AUUUUUUGHHH'
+                    'AUUUUUUGHHH',
+                    'Custom Sound',
+                    'Check for updates'
                 ],
             });
 
             function replaceSound(sound, soundName) {
                 console.log(`${chalk.bgBlueBright('info')} You selected ${soundName}`)
+                if (fs.existsSync(`sounds`)) {
+                    console.log(`${chalk.bgBlueBright('info')} Sounds folder already downloaded`);
+                } else {
+                    console.log(`${chalk.bgBlueBright('info')} Downloading sounds...`);
+                    download(url, './', { extract: true })
+                        .then(() => {
+                            console.log(`${chalk.bgGreenBright('success')} Sounds downloaded!`);
+                        }).catch(err => {
+                            console.log(`${chalk.bgRedBright('error')} ${err}`);
+                        });
+                }
                 try {
-                    if(fs.existsSync(`${sounds}\\ouch.ogg`)) {
+                    if (fs.existsSync(`${sounds}\\ouch.ogg`)) {
                         console.log(`${chalk.bgBlueBright('info')} Found oof sound`);
                         fs.unlink(`${sounds}\\ouch.ogg`, (err) => {
                             if (err) {
@@ -40,10 +57,11 @@ fs.readdirSync(`${process.env.LOCALAPPDATA}\\Roblox\\Versions`).forEach(file => 
                                     throw err;
                                 }
                                 console.log(`${chalk.bgGreenBright('success')} Enjoy your new oof sound!`);
+                                console.log(`${chalk.bgYellowBright('note')} If Roblox is already open, you will need to restart it for the new death sound to take effect`);
                                 console.log('Exiting in 5 seconds')
-                            setTimeout(function(){
-                                console.log("Goodbye");
-                            }, 5000);
+                                setTimeout(function () {
+                                    console.log("Goodbye");
+                                }, 5000);
                             });
                         });
                     } else {
@@ -53,8 +71,9 @@ fs.readdirSync(`${process.env.LOCALAPPDATA}\\Roblox\\Versions`).forEach(file => 
                                 throw err;
                             }
                             console.log(`${chalk.bgGreenBright('success')} Enjoy your new oof sound!`);
+                            console.log(`${chalk.bgYellowBright('note')} If Roblox is already open, you will need to restart it for the new death sound to take effect`);
                             console.log('Exiting in 5 seconds')
-                            setTimeout(function(){
+                            setTimeout(function () {
                                 console.log("Goodbye");
                             }, 5000);
                         });
@@ -62,7 +81,7 @@ fs.readdirSync(`${process.env.LOCALAPPDATA}\\Roblox\\Versions`).forEach(file => 
                 } catch (err) {
                     console.error(err);
                 }
-                
+
             }
 
             if (answers.playSelect === 'Oof') {
@@ -79,13 +98,40 @@ fs.readdirSync(`${process.env.LOCALAPPDATA}\\Roblox\\Versions`).forEach(file => 
                 replaceSound('yoda', 'Lego Yoda Death')
             } else if (answers.playSelect === 'AUUUUUUGHHH') {
                 replaceSound('augh', 'AUUUUUUGHHH')
+            } else if (answers.playSelect === 'Custom Sound') {
+                console.log(`${chalk.bgBlueBright('info')} You selected Custom Sound`)
+                console.log(`${chalk.bgYellowBright('info')} Place your OGG sound file in the "sounds\\custom" folder and rename the OGG file to ouch.ogg`)
+                const answers = await inquirer.prompt({
+                    name: 'confirmCustom',
+                    type: 'confirm',
+                    message: 'Say "yes" once you have placed the OGG file in the "sounds\\custom" folder',
+                    default: true,
+                });
+                replaceSound('custom', 'Custom Sound')
+            } else if (answers.playSelect === 'Check for updates') {
+                axios
+                    .get('https://raw.githubusercontent.com/SegoGithub/oof-is-back/main/package.json')
+                    .then(res => {
+                        if (res.data.version !== version) {
+                            console.log(chalk.yellowBright(`New version available! ${version} => ${res.data.version}`));
+                            console.log(chalk.yellowBright(`Download link: https://github.com/SegoGithub/oof-is-back/releases`));
+                            setTimeout(function () {
+                                selectSound();
+                            }, 10000);
+                        } else if (res.data.version === version) {
+                            console.log(chalk.greenBright(`You are running the latest version!`));
+                            setTimeout(function () {
+                                selectSound();
+                            }, 10000);
+                        }
+                    })
+                    .catch(error => {
+                        console.error(error);
+                    });
             }
 
         }
-        
-        
-        
         selectSound()
     }
-  });
+});
 
